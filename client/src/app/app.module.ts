0b81473a9c387803303a2e157c7c9a534f13bab2
sdediego <sergio.dediego@outlook.com>
2017-12-05 13:51:55 +0100

    Created login and signup components (not implemented). Created folder structure. Implemented user interface and environment

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
