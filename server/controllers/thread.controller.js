9e6b760bd2f38a4b18ad3b0f49de473a1e82a8db
sdediego <sergio.dediego@outlook.com>
2017-12-07 17:37:25 +0100

    Remove thread and referenced docs (replies and votes)

--- server/controllers/thread.controller.js
+++ server/controllers/thread.controller.js
@@ -16,25 +16,39 @@ module.exports.thread = (req, res, next) => {
         return;
       }
 
-      Reply.find({ thread: threadId })
-        .populate('author', 'username')
-        .exec()
-        .then(replies => {
+      const toSearch = [
+        Reply.find({ thread: threadId }).populate('author', 'username'),
+        Vote.find({ thread: threadId })
+      ];
+
+      Promise.all(toSearch)
+        .then(values => {
+          const replies = values[0];
+          const votes = values[1].length;
+
           if (!replies) {
             res.status(200).json({
               msg: 'Unable to retrieve replies for this thread.',
-              thread: thread
+              thread: thread,
+              votes: votes
             });
           }
 
-          res.status(200).json({ thread, replies });
+          if (!votes) {
+            res.status(200).json({
+              msg: 'Unable to retrieve votes for this thread.',
+              thread: thread,
+              replies: replies
+            });
+          }
+
+          res.status(200).json({ thread, replies, votes });
         })
         .catch(error => {
           res.status(500).json({
-            msg: 'Unable to retrieve thread replies.',
-            thread: thread
+            msg: 'Error while resolving promise.',
+            error: error.message
           });
-          return;
         });
     })
     .catch(error => {
@@ -69,6 +83,66 @@ module.exports.newThread = (req, res, next) => {
     });
 };
 
+module.exports.removeThread = (req, res, next) => {
+  const threadId = req.params.id;
+
+  Thread.findById(threadId)
+    .populate('author')
+    .exec((error, thread) => {
+      if (error) {
+        res.status(500).json({
+          msg: 'Error fetching thread.',
+          error: error
+        });
+        return;
+      }
+
+      if (!thread) {
+        res.status(404).json({ msg: 'Thread not found.' });
+        return;
+      }
+
+      if (thread.author.id !== req.user.id) {
+        res.status(403).json({ msg: 'Unallowed to remove this thread.' });
+        return;
+      } else {
+        const toRemove = [
+          Thread.findByIdAndRemove(threadId),
+          Reply.find({ thread: threadId }).remove(),
+          Vote.find({ thread: threadId }).remove()
+        ];
+
+        Promise.all(toRemove)
+          .then(result => {
+            const thread = result[0];
+            const replies = result[1];
+            const votes = result[2];
+
+            res.status(200).json({
+              msg: 'Thread and reference docs successfully removed.',
+              thread: thread,
+              reply: replies,
+              votes: votes
+            });
+          })
+          .catch(error => {
+            thread.remove()
+              .then(thread => {
+                res.status(206).json({
+                  msg: 'Thread removed. Referenced docs remain intact.',
+                });
+              })
+              .catch(error => {
+                res.status(500).json({
+                  msg: 'Error resolving promises simultaneously.',
+                  error: error
+                });
+              });
+          });
+      }
+    });
+};
+
 module.exports.reply = (req, res, next) => {
   const newReply = new Reply({
     author: req.user.id,
@@ -87,6 +161,10 @@ module.exports.reply = (req, res, next) => {
       res.status(500).json({ msg: 'Unable to save new reply.' });
       return;
     });
+};
+
+module.exports.removeReply = (req, res, next) => {
+
 };
 
 module.exports.vote = (req, res, next) => {
--- server/routes/api/thread.routes.js
+++ server/routes/api/thread.routes.js
@@ -4,6 +4,7 @@ const threadController = require('./../../controllers/thread.controller');
 
 router.post('/new', threadController.newThread);
 router.get('/:id', threadController.thread);
+router.get('/:id/remove', threadController.removeThread);
 router.post('/:id/reply', threadController.reply);
 router.post('/:id/vote', threadController.vote);
 
