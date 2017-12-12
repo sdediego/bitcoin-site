b530ff66e1fa582a8136c81e1eb48d4202cdc9ef
sdediego <sergio.dediego@outlook.com>
2017-12-12 18:11:02 +0100

    Chart completed

--- client/src/app/components/chart/chart.component.ts
+++ client/src/app/components/chart/chart.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Component, OnInit, OnChanges, AfterViewInit, ViewChild } from '@angular/core';
 
 import { Observable } from 'rxjs/Rx';
+import { BaseChartDirective } from 'ng2-charts/ng2-charts';
 
 import { IUser } from './../../shared/interfaces/user.interface';
 import { AuthService } from './../../shared/services/auth.service';
@@ -12,12 +13,15 @@ import { CoindeskService } from './../../shared/services/coindesk.service';
   templateUrl: './chart.component.html',
   styleUrls: ['./chart.component.css']
 })
-export class ChartComponent implements OnInit, AfterViewInit {
+export class ChartComponent implements OnInit, OnChanges, AfterViewInit {
+
+  @ViewChild(BaseChartDirective) graphic: BaseChartDirective;
 
   public user: IUser;
   public btcPriceHist: object;
   public dates: Array<any>;
   public printData: Array<number>;
+  public printDates: Array<any>;
   public isDecimal: boolean = true;
   public btcPriceSeries: Array<number>;
   public btcLogPriceSeries: Array<number>;
@@ -42,39 +46,47 @@ export class ChartComponent implements OnInit, AfterViewInit {
 
   public ngOnInit(): void {}
 
+  public ngOnChanges(): void {
+    this.printGraphic();
+  }
+
   public ngAfterViewInit(): void {
     this.getBtcPrice();
   }
 
-  public getBtcPrice() {
+  public getBtcPrice(): void {
     this.coindeskService.getHistoricalBtcPrice().map(
       response => {
         this.btcPriceHist = response['bpi'];
         this.dates = Object.keys(this.btcPriceHist);
         this.btcPriceSeries = Object.values(this.btcPriceHist);
-        return this.btcPriceHist, this.btcPriceSeries, this.dates;
+        this.btcLogPriceSeries = this.btcPriceSeries.map(price => {
+          return Math.log(price) > 0 ? Math.log(price) : 0;
+        });
       }).
       subscribe(
         response => {
           this.printData = this.btcPriceSeries;
+          this.printDates = this.dates;
           this.lineChartData = [
             { data: this.printData, label: 'BTC' }
           ];
-          this.lineChartLabels = this.dates;
+          this.lineChartLabels = this.printDates;
           this.lineChartOptions = {
             responsive: false,
           };
           this.lineChartColors = [
             {
-              backgroundColor: 'rgba(148,159,177,0.2)',
-              borderColor: 'rgba(148,159,177,1)',
-              pointBackgroundColor: 'rgba(148,159,177,1)',
-              pointBorderColor: '#fff',
-              pointHoverBackgroundColor: '#fff',
-              pointHoverBorderColor: 'rgba(148,159,177,0.8)'
+              //backgroundColor: '#fab915',
+              borderColor: '#fab915',
+              //pointBackgroundColor: 'rgba(148,159,177,1)',
+              pointRadius: 0,
+              //pointBorderColor: '#fff',
+              //pointHoverBackgroundColor: '#fff',
+              //pointHoverBorderColor: 'rgba(148,159,177,0.8)'
             }
           ];
-          this.lineChartLegend = true;
+          this.lineChartLegend = false;
           this.lineChartType = 'line';
         },
         error => {
@@ -83,34 +95,48 @@ export class ChartComponent implements OnInit, AfterViewInit {
   }
 
   public toLogScale(): void {
-    this.btcLogPriceSeries = this.btcPriceSeries.map(price => {
-      return Math.log(price) > 0 ? Math.log(price) : 0;
-    });
-    this.printData = this.btcLogPriceSeries;
-    this.isDecimal = false;
-    this.printGraphic();
+    if (this.isDecimal) {
+      this.printData = this.printData.map(price => {
+        return Math.log(price) > 0 ? Math.log(price) : 0;
+      });
+      this.isDecimal = false;
+      this.printGraphic();
+    }
   }
 
   public toDecimalScale(): void {
-    this.printData = this.btcPriceSeries;
-    this.isDecimal = true;
-    this.printGraphic();
+    if (!this.isDecimal) {
+      this.printData = this.printData.map(price => {
+        return Math.exp(price);
+      });
+      this.isDecimal = true;
+      this.printGraphic();
+    }
   }
 
   public toTimeInterval(days): void {
     let series = this.isDecimal ? this.btcPriceSeries : this.btcLogPriceSeries;
+    let seriesDates = this.dates;
+    console.log(series, seriesDates);
     if (!days) {
       this.printData = series;
+      this.printDates = this.dates;
     } else {
       this.printData = series.slice(series.length - days, series.length);
+      this.printDates = seriesDates.slice(seriesDates.length - days, seriesDates.length);
     }
     this.printGraphic();
+  }
+
+  public calculateMediumAverage(days) {
+
   }
 
   public printGraphic() {
     this.lineChartData = [
       { data: this.printData, label: 'BTC' }
     ];
+    this.graphic.chart.config.data.labels = this.printDates;
   }
 
 }
