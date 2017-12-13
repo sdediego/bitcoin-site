38ae37548c48839646b46d4912733f3b977df649
sdediego <sergio.dediego@outlook.com>
2017-12-13 18:27:41 +0100

    Minor changes for deploy: Public

--- server/app.js
+++ server/app.js
@@ -53,7 +53,7 @@ app.use(passport.session());
 app.use('/', indexRoutes);
 
 app.use(function(req, res) {
-  res.sendfile(__dirname + '/public/index.html');
+  req.sendFile(__dirname + '/public/index.html');
 });
 
 // catch 404 and forward to error handler
