cd0c9531f2c5b2dc625e0cf3ab2e50ef903bcfcb
sdediego <sergio.dediego@outlook.com>
2017-12-14 01:34:37 +0100

    Subscribe service created

---
+++ client/src/app/shared/services/subscribe.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+
+import { Observable } from 'rxjs/Rx';
+
+import { IUser } from './../interfaces/user.interface';
+import { environment } from './../../../environments/environment';
+
+
+@Injectable()
+export class SubscribeService {
+
+  private user: IUser;
+  private baseUrl = `${environment.apiUrl}`;
+  private headers = new Headers({ 'Content-Type': 'application/json' });
+  private options = new RequestOptions({ headers: this.headers, withCredentials: true });
+
+  constructor(private http: Http) {}
+
+  private handleError(error: Response | any): Observable<string> {
+    return Observable.throw(error.json().message);
+  }
+
+  public sendSubscritption(email: string): Observable<any> {
+    return this.http.post(`${this.baseUrl}/subscription`, JSON.stringify(email), this.options)
+      .map((res: Response) => res.json())
+      .catch((error: Response | any) => this.handleError(error));
+  }
+
+}
